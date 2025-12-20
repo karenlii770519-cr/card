63303d32d8d3bd6bfb68684125d431f34ae6230f
karenlii770519-cr <karenlii770519@gmail.com>
2025-12-20 22:36:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/card/', // 這裡必須對應你的 GitHub Repo 名稱
+  base: './', 
   build: {
     outDir: 'dist',
   },
